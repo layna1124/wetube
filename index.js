8a163bcf6faf688c783e541aab5595f559af50d8
unknown <layna1124@gmail.com>
2019-01-29 18:31:12 +0900

    express base

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("test message")
+const express = require('express');
+const app = express();
+ 
+const PORT = 3000;
+function handleListening(){
+ console.log("listening on : http://localhost:${PORT}")
+}
+
+function handleHome(req, res){
+  console.log(req);
+  res.send('hello from home')
+}
+function handleProfile(req,res){
+  res.send("profile page , routes.. html css file")
+}
+
+app.get("/", handleHome);
+app.get("/profile",handleProfile);
+
+app.listen(PORT,handleListening);
